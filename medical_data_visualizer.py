b633ff1c0eb0d558b09ff44fbedd4528c2efd7bc
blake long <long.blake.k@gmail.com>
2025-03-25 03:31:18 +0000

    03/24/2025 10:31 pm

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,34 +1,30 @@
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = df['weight'].apply(lambda x: 1 if x>25 else 0)
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x<=1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x<=1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.rename(columns={'value': 'category'})
+    df_cat = df_cat.groupby(['cardio', 'variable', 'category']).size().reset_index(name='count')
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
+    # 7 & 8
+    fig = sns.catplot(x='variable', y='count', hue='category', col='cardio', kind='bar', data=df_cat)
 
     # 9
     fig.savefig('catplot.png')
@@ -38,7 +34,9 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df
+    if df_heat.iloc['ap_hi'] >= df_heat.iloc['ap_lo']:
+        pass
 
     # 12
     corr = None
@@ -49,12 +47,15 @@ def draw_heat_map():
 
 
     # 14
-    fig, ax = None
+
+    #fig, ax = None
 
     # 15
 
 
 
     # 16
-    fig.savefig('heatmap.png')
-    return fig
+
+    #fig.savefig('heatmap.png')
+    #return fig
+
